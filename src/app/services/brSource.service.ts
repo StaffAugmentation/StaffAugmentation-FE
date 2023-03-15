1ec0828458f7e5333f5a8c5807885c7f55cdea14
Alae Benhamza <alaebenhamza3@gmail.com>
2023-03-15 17:54:08 +0100

    BrSource

---
+++ src/app/services/brSource.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BrSource } from '@models/brSource';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BrSourceService {
+
+  private baseUrl : string = `${environment.baseUrl}api/BrSource`;
+
+  constructor(private http: HttpClient) { }
+
+  public getAll() : Observable<BrSource[]>{
+    return this.http.get<BrSource[]>(this.baseUrl);
+  }
+
+  public getOne(idSource: number) : Observable<BrSource>{
+    return this.http.get<BrSource>(`${this.baseUrl}/${idSource}`);
+  }
+
+  public addBrSource(brSource: BrSource) : Observable<BrSource>{
+    return this.http.post<BrSource>(`${this.baseUrl}`,brSource);
+  }
+
+  public updateBrSource(brSource: BrSource) : Observable<BrSource>{
+    return this.http.put<BrSource>(`${this.baseUrl}`,brSource);
+  }
+  public deleteBrSource(idSource: number) : Observable<BrSource>{
+    return this.http.delete<BrSource>(`${this.baseUrl}/${idSource}`);
+  }
+}
