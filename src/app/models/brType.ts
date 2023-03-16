3edf8cdd3708e1f36aa6766929a0f6064f2b39c2
Alae Benhamza <alaebenhamza3@gmail.com>
2023-03-17 00:27:17 +0100

    BrType

---
+++ src/app/models/brType.ts
@@ -0,0 +1,15 @@
+export class BrType{
+  id: number;
+  valueId: number;
+  isActive: boolean;
+  constructor
+      (
+      id: number,
+      valueId: number,
+      isActive: boolean,
+      ){
+          this.id=id,
+          this.valueId=valueId,
+          this.isActive=isActive
+      }
+}
