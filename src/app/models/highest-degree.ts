6de426db622fd1e39a003d857adda2c796fdff85
Alae Benhamza <alaebenhamza3@gmail.com>
2023-04-03 16:54:40 +0000

    highest degree modification

--- src/app/models/highest-degree.ts
+++ src/app/models/highest-degree.ts
@@ -1,12 +1,15 @@
 export class HighestDegree{
   id: number;
   value: string;
+  isActive: boolean;
   constructor
       (
       id: number,
       value: string,
+      isActive: boolean,
       ){
           this.id=id,
-          this.value=value
+          this.value=value,
+          this.isActive=isActive
       }
 }
